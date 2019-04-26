84d4eb0fd63b2a2e9fab9908af5a078ddc9bebfd
funchest <45046290+funchest@users.noreply.github.com>
2019-04-26 15:35:22 +0100

    Add files via upload

---
+++ src/main/java/com/example/demo/bookRepo.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface bookRepo extends CrudRepository<book, Integer> {
+
+}
---
+++ src/main/java/com/example/demo/commentRepo.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface commentRepo extends CrudRepository<comment, Integer> {
+
+}
